c9451502641a40b1d1be54c6800aba643c3a4b92
Urvi Garg <urvigarg139@gmail.com>
2026-09-23 22:09:06 +0530

    Implement rearrangement of array by sign

---
+++ Rearrange_Array_elements_bysign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n);
+
+        int pos = 0;  // position for positive numbers
+        int neg = 1;  // position for negative numbers
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+            else {
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+        }
+
+        return ans;
+    }
+};
